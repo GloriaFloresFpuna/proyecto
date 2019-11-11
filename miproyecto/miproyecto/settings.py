4cf0287ea4f5b086366213631af3b9554e926d66
GloriaFloresFpuna <floresperanzasoy@gmail.com>
2019-11-12 07:34:18 +0900

    Se añadieron nuevos archivos

--- miproyecto/miproyecto/settings.py
+++ miproyecto/miproyecto/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,10 +75,19 @@ WSGI_APPLICATION = 'miproyecto.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+	
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'proyecto2019',
+		'USER': 'postgres',
+		'PASSWORD': '12345',
+		'HOST': 'localhost',
+		'PORT': '',
+	}
 }
 
 
